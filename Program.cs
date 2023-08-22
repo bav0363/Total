f3f91865cc207fee70627d0f0273642bd58efddd
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-08-22 21:09:36 +0300

    Создал основной метод программы

--- Program.cs
+++ Program.cs
@@ -9,3 +9,41 @@
     return array;
 }
 
+void ShowArray(string[] array)
+{   
+    Console.Write("[{0}]", string.Join(", ", array));
+    Console.WriteLine();    
+}
+
+string[] Treatment(string[] array)
+{
+    int count = 0;    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {            
+            count ++;
+        }        
+    }
+    string[] new_array = new string[count];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            new_array[index] = array[i];
+            index ++;
+        }        
+    }
+    return new_array;
+} 
+
+
+Console.WriteLine("Сколько элементов вы хотите записать в массив?");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] my_array = Input(size);
+Console.WriteLine("Начальный массив:");
+ShowArray(my_array);
+string[] new_array = Treatment(my_array);
+Console.WriteLine("Обработанный массив:");
+ShowArray(new_array);
