163cdd154536906b6274e9553eb653312523e2b4
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-08-22 20:41:51 +0300

    Создал метод запроса данных у пользователя

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] Input(int size)
+{
+    string[] array = new string[size];    
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"Введите [{index + 1}] элемент массива: ");
+        array[index] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
